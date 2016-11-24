2dfd528c37f22f21e5991c208074adabe593d1c5
José Claudio <JPereira1330@gmail.com>
2016-11-24 18:24:28 -0200

    Update TempFreezer.sh

--- TempFreezer.sh
+++ TempFreezer.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
 # Programador: José Claudio Pereira <Nyex>
-# TempFreezer - Ver: 0.0.3 Dev
-
+# TempFreezer - Ver: 0.0.4 Dev
 
 ###########################
 # CONFIGURAÇÕES DO SCRIPT #
 ###########################
 
-# Diretorio onde sera armazenado os dados do contador
-diretorio=/etc/TempFreezer/contador
-
-# Array de diretorio que não seram apagados
-manterArq[0]=/home/biblioteca/Área\ de\ Trabalho/Salvar\ dados
-manterArq[1]=/home/biblioteca/Downloads
-
-# Quantia de inicializações do sistema para o reset
-vezesIniciado=5
+diretorio="/etc/TempFreezer"                # Diretorio onde sera armazenado os dados do script
+contador=$diretorio"/contador.txt"          # Arquivo onde sera salvo a contagem
+userBackup=$diretorio"/backup.tar.gz"       # Pasta do usuario zipada em tar.gz 
+manterArq[0]="/home/biblioteca/Downloads"   # Array de diretorio que não seram apagados
+vezesIniciado=5                             # Quantia de inicializações do sistema para o reset
 
 ####################
 # INICIO DO SCRIPT #
 ####################
 
 # Verifica se arquivo de contagem existe.
-if [ -f $diretorio ];
-then
+if [ -d $diretorio -o -f $contador ]; then
 	
-	linha=$(head $diretorio) # Captura primeira linha do contador
+	linha=$(head $contador) # Captura primeira linha do contador
 
 	if [ $linha -gt $vezesIniciado -o $linha -eq $vezesIniciado ]; # Se 'linha' for maior ou igual do que 'vezesiniciado'
 	then
-		echo "Maior ou igual"
+        echo "teste"
 	else
 		linha_new=$(($linha + 1))
-		sed -i 's/'"$linha"'/'"$linha_new"'/g' $diretorio
+		sed -i 's/'"$linha"'/'"$linha_new"'/g' $contador
 	fi
 else
-	
-	echo "1" >> $diretorio
-	chmod 755 $diretorio
+
+    # Cria uma pasta caso nao exista
+	if [ ! -d $diretorio ]; then
+        mkdir $diretorio
+    fi
+
+    # Cria o arquivo e inicializa ele com 1 caso não exista
+	echo "1" >> $contador
+	chmod 755 $contador
+
 fi
