9c54d6ea8cd129e602dec899fd043586a98201e4
José Claudio <JPereira1330@gmail.com>
2016-11-29 19:07:27 -0200

    Update TempFreezer.sh

--- TempFreezer.sh
+++ TempFreezer.sh
@@ -10,9 +10,9 @@
 diretorio="/etc/TempFreezer"                # Caminho onde sera armazenado os dados do script
 contador=$diretorio"/contador.txt"          # Arquivo onde sera salvo a contagem
 userBackup=$diretorio"/backup"              # Pasta onde fica toda 
-userDir="/home/biblioteca"            # Caminho do diretorio que sera apagado
+userDir="/home/biblioteca"           	    # Caminho do diretorio que sera apagado
 manterPasta="/Downloads"                    # Diretorio que não sera apagado
-usuario="biblioteca"						# Nome do usuario
+usuario="biblioteca"			    # Nome do usuario
 vezesIniciado=5                             # Quantia de inicializações do sistema para o reset
 
 ####################
@@ -22,29 +22,29 @@ vezesIniciado=5                             # Quantia de inicializações do sis
 # Verifica se arquivo de contagem existe.
 if [ -d $diretorio -o -f $contador ]; then
 	
-    # Captura primeira linha do contador
+    	# Captura primeira linha do contador
 	linha=$(head $contador)
 
-    # Se 'linha' for maior ou igual do que 'vezesiniciado'
+    	# Se 'linha' for maior ou igual do que 'vezesiniciado'
 	if [ $linha -gt $vezesIniciado -o $linha -eq $vezesIniciado ];
 	then
-        rm -r $userDir!($manterArq)	# Deleta pasta do usuario
-        cp -r $userBackup /home/	# copia a pasta backup do usuario para home
-		chown $usuario $userDir		# muda dono no diretorio
+		rm -r $userDir!($manterArq)	# Deleta pasta do usuario
+        	cp -R $userBackup /home/	# Copia a pasta backup do usuario para home
+		chown -R $usuario $userDir	# Altera dono no diretorio
+		chgrp -R $usuario $usuaDir	# Altera o grupo do diretorio
+		sed -i 's/'"$linha"'/'"1"'/g' $contador
 	else
 		linha_new=$(($linha + 1))
 		sed -i 's/'"$linha"'/'"$linha_new"'/g' $contador
 		cp -r $userBackup /home/	# Manter as configurações padrões
 	fi
 else
-
-    # Cria uma pasta caso nao exista
+   	# Cria uma pasta caso nao exista
 	if [ ! -d $diretorio ]; then
-        mkdir $diretorio
-    fi
+        	mkdir $diretorio
+	fi
 
-    # Cria o arquivo e inicializa ele com 1 caso não exista
+    	# Cria o arquivo e inicializa ele com 1 caso não exista
 	echo "1" >> $contador
 	chmod 755 $contador
-
 fi
