c885b2af6ad038a07c0098b4d3f230e9ad576f81
Nyex <JPereira1330@gmail.com>
2017-07-10 22:40:18 -0300

    Instalação do servidor

---
+++ Server/installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# @Author: JPereira1330
+# @Date: 09/07/17
+# @Version: 3.0
+#
+
+echo"
+  _|_|_|_|_|  _|_|_|_|     |
+      _|      _|           | TempFreezer
+      _|      _|_|_|       | Version: 3.0
+      _|      _|           | Server installer
+      _|      _|           |
+
+  Want to continue the installation? [S/n] (Default: 'S')
+  ->
+"
